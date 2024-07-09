6ebea9e4f0725c1d9ee0b030e1d7504d92c258bd
bruno santos <brunosantos.cps@gmail.com>
2024-07-09 11:57:31 -0300

    adding addhotel page and e2e tests

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,66 @@
+import express, { Request, Response } from "express";
+import multer from 'multer'
+import cloudinary from 'cloudinary'
+import Hotel, { HotelType } from "../models/hotel";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator"
+import { timeStamp } from "console";
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1025
+  }
+})
+
+router.post("/",
+  verifyToken,
+  [
+    body("name").notEmpty().withMessage('Name is required'),
+    body("city").notEmpty().withMessage('City is required'),
+    body("county").notEmpty().withMessage('County is required'),
+    body("description").notEmpty().withMessage('Description is required'),
+    body("type").notEmpty().withMessage('Hotel type is required'),
+    body("pricePerNight").notEmpty().isNumeric().withMessage('Price per night is required and must be a number'),
+    body("facilities").notEmpty().isArray().withMessage('Facilities are required'),
+  ],
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[]
+      const newHotel: HotelType = req.body;
+
+      const imageUrls = await uploadImages(imageFiles)
+      //2. if upload success, add the URLS to the newHotel
+      newHotel.imageUrls = imageUrls
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId
+
+      //3. save the new hotel in our database
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
+
+      //4. return 201 status
+      res.status(201).json(hotel)
+    } catch (error) {
+      console.log("Error creating hotel", error)
+      res.status(500).json({ message: "Something went wrong" })
+    }
+  })
+
+
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
+
+export default router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import AddHotel from './pages/AddHotel'
+import { useAppContext } from './contexts/AppContext'
 
 function App() {
 
-
+  const { isLoggedIn } = useAppContext();
   return (
     <BrowserRouter>
       <Routes>
@@ -18,7 +20,9 @@ function App() {
         <Route path="/register" element={<Layout><Register></Register></Layout>} />
         <Route path="/sign-in" element={<Layout><Login></Login></Layout>} />
 
-
+        {isLoggedIn && (<>
+          <Route path='/add-hotel' element={<Layout><AddHotel></AddHotel></Layout>} />
+        </>)}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
